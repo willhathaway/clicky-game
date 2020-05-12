fffef10de36777bc7b40f43f6f88f7b2eec9e143
Will Hathaway <william.hathaway38@gmail.com>
2020-05-12 11:48:46 -0400

    formatting changes to App.js 2

--- src/App.js
+++ src/App.js
@@ -94,17 +94,17 @@ class App extends Component {
                     currentScore={this.state.currentScore}
                     hiScore={this.state.hiScore}
                 />
-                
-                    {this.state.imageArray.map(image => (
-                        <ImageCard
-                            id={image.id}
-                            key={image.id}
-                            image={image.image}
-                            click={this.click}
-                        />
 
-                    ))}
-               
+                {this.state.imageArray.map(image => (
+                    <ImageCard
+                        id={image.id}
+                        key={image.id}
+                        image={image.image}
+                        click={this.click}
+                    />
+
+                ))}
+
             </Wrapper>
         )
     }
