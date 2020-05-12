e5bf3054b883c5fbb5c292ddb58d14a484f6ce26
Will Hathaway <william.hathaway38@gmail.com>
2020-05-12 11:49:53 -0400

    formatting changes to Scores index.js

--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function ImageCard(props) {
     return (
         <div className="card">
-            <div className="img-container" onClick = {() => props.click(props.id)}>
+            <div className="img-container" onClick={() => props.click(props.id)}>
                 <img alt={props.name} src={props.image} />
             </div>
         </div>
